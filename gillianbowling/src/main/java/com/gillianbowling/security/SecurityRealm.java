f8b817b7e88de7409bef948095cf323f7c7127b9
Terrence Curran <tcurran@approachingpi.com>
2015-02-14 19:18:59 -0500

    CDI security realm working

--- gillianbowling/src/main/java/com/gillianbowling/security/SecurityRealm.java
+++ gillianbowling/src/main/java/com/gillianbowling/security/SecurityRealm.java
@@ -1,13 +1,20 @@
 package com.gillianbowling.security;
 
+import com.gillianbowling.data.model.Account;
 import com.gillianbowling.data.repositories.AccountRepository;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
@@ -22,11 +29,13 @@ import java.util.Set;
  */
 public class SecurityRealm extends AuthorizingRealm {
 
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityRealm.class);
 
+	protected AuthenticationInfo doGetAuthenticationInfo(final AuthenticationToken inToken) {
+
+		// Try to get the account manager from CDI
 		BeanManager beanManager = CDI.current().getBeanManager();
 		Set<Bean<?>> accountRepositories = beanManager.getBeans(AccountRepository.class);
-
 		AccountRepository accountRepository = null;
 		if (accountRepositories != null) {
 			Bean<AccountRepository> accountRepositoryBean = (Bean<AccountRepository>) beanManager
@@ -37,14 +46,25 @@ public class SecurityRealm extends AuthorizingRealm {
 					.getReference(accountRepositoryBean, AccountRepository.class, ctx);
 		}
 
-		if (accountRepository != null) {
-			accountRepository.findAnyByUsername((String) token.getPrincipal());
+		if (accountRepository == null) {
+			throw new AuthenticationException("Unable to get accountRepository from CDI");
 		}
 
-		SimpleAuthenticationInfo authn = new SimpleAuthenticationInfo(token.getPrincipal(),
-				token.getCredentials(), "myRealm");
 
-		return authn;
+		UsernamePasswordToken upToken = (UsernamePasswordToken)inToken;
+  		String username = upToken.getUsername();
+
+		Account account = accountRepository.findAnyByUsername(username);
+		if (account == null) {
+			throw new UnknownAccountException("No account found for user [" + username + "]");
+		}
+
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(username, account.getPassword(), getName());
+		if (account.getSalt() != null) {
+			info.setCredentialsSalt(ByteSource.Util.bytes(account.getSalt()));
+		}
+
+		return info;
 	}
 
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
